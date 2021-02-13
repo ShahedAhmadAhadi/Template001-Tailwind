535da0adbcc5ee68aae524a8e483b6381910341b
Shahed Ahmad <shaheda.a16@gmail.com>
2021-02-13 23:31:33 +0430

    file minified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ['./build/index.html'],
+    defaultExtractor: content => content.match[/[A-Za-z0-9-_:/]+/g] || []
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
